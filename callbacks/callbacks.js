8dd775b355b10d0f3d29a3adb5b5b1d50af69cf2
Tessa Kelly <tessakkelly@gmail.com>
2015-04-26 09:06:52 -0700

    solve callbacks.js

--- callbacks/callbacks.js
+++ callbacks/callbacks.js
@@ -1,1 +1,25 @@
 // Complete your callback exercises below
+
+// 1
+
+var funcCaller = function(func, arg) {
+  return func(arg);
+};
+
+// 2
+
+var firstVal = function(arr, func) {
+  func(arr[0], 0, arr);
+};
+
+// 3
+
+var firstVal = function(list, func) {
+  if(Array.isArray(list)) {
+    func(list[0], 0, list);
+  } else {
+    for(var key in list) {
+      return func(list[key], key, list);
+    }
+  }
+};
