cb78b2468a5c29a57331602519e24bc8db7b292e
Tessa Kelly <tessakkelly@gmail.com>
2015-04-26 17:23:15 -0700

    solve underscore.js

--- underscore-loops/underscore.js
+++ underscore-loops/underscore.js
@@ -1,1 +1,62 @@
 // Complete your underscore exercises below
+
+// 1
+
+_.each([1,2,3], function(val) { console.log(val); });
+
+_.each([1,2,3], function(val, index) { console.log(index); });
+
+// 2
+
+var checkValue = function(arr, val) {
+  var found = false;
+  for(var i = 0; i < arr.length; i++) {
+    if(val === arr[i]) {
+      found = true;
+    }
+  }
+  return found;
+}
+
+// 3
+
+var checkValue = function(arr, val) {
+  var found = false;
+  _.each(arr, function(value) {
+    if(value === val) {
+      found = true;
+    }
+  });
+  return found;
+};
+
+// 4
+
+/* The instructions don't ask you to create functions here, 
+   so no worries if you didn't! Just make sure your logic is similar */
+
+var objPusher = function(obj) {
+  var arr = [];
+  for(var key in obj) {
+    arr.push(obj[key]);
+  }
+  return arr;
+}
+
+// 5
+
+var objPusher = function(obj) {
+  return _.map(obj, function(val) {
+    return val;
+  });
+};
+
+// 6
+
+var evensOnly = function(collection) {
+  return _.filter(collection, function(val) {
+    return !(val % 2);
+  });
+};
+
+
